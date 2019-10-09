f185f43c0bec97fa36c3ca2027e78f463fa14b34
srikanth97 <nksr.1997@gmail.com>
2019-10-09 11:46:06 +0530

    implemented three interfaces

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,12 +1,18 @@
 package com.stackroute.domain;
 
 import com.stackroute.domain.Actor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
     @Autowired
     @Qualifier("actor1")
     private Actor actor;
@@ -16,5 +22,21 @@ public class Movie {
         return "Actor name : " +actor.getName() +
                 " Actor age : " + actor.getAge() +
                 " Actor gender :- " + actor.getGender();
+    }
+
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(beanFactory);
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Bean : " + s);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(applicationContext);
     }
 }
